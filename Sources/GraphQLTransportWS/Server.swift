aa87c044210759b7a7104f13de70dc7aa6822a8b
Jay Herron <NeedleInAJayStack@protonmail.com>
2023-02-02 21:33:33 -0700

    feature: Allows capture of Server errors

--- Sources/GraphQLTransportWS/Server.swift
+++ Sources/GraphQLTransportWS/Server.swift
@@ -17,7 +17,7 @@ public class Server<InitPayload: Equatable & Codable> {
     
     var onExit: () -> Void = { }
     var onOperationComplete: (String) -> Void = { _ in }
-    var onOperationError: (String) -> Void = { _ in }
+    var onOperationError: (String, [Error]) -> Void = { _, _ in }
     var onMessage: (String) -> Void = { _ in }
     
     var initialized = false
@@ -120,7 +120,7 @@ public class Server<InitPayload: Equatable & Codable> {
     
     /// Define the callback to run on error of any full operation (failed query, interrupted subscription)
     /// - Parameter callback: The callback to assign, taking a string parameter for the ID of the operation
-    public func onOperationError(_ callback: @escaping (String) -> Void) {
+    public func onOperationError(_ callback: @escaping (String, [Error]) -> Void) {
         self.onOperationError = callback
     }
     
@@ -255,7 +255,7 @@ public class Server<InitPayload: Equatable & Codable> {
                 id: id
             ).toJSON(encoder)
         )
-        self.onOperationError(id)
+        self.onOperationError(id, errors)
     }
     
     /// Send an `error` response through the messenger
